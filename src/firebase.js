06daecc883059ba5e34dac1e2ab1e063a9ba2400
dracu-lah <nevilnics4321@gmail.com>
2022-10-18 16:20:52 +0530

    created frontend

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC05eI_JoWwxBgIvH3buwGo2tcAJNeB86A",
+    authDomain: "netflix-clone-5d1ed.firebaseapp.com",
+    projectId: "netflix-clone-5d1ed",
+    storageBucket: "netflix-clone-5d1ed.appspot.com",
+    messagingSenderId: "224017651936",
+    appId: "1:224017651936:web:dd468fdd6267538aaa1885"
+  };
